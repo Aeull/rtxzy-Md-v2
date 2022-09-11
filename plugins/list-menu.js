694398882c749ef20319d0ce15209a55f067e5c9
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-11 20:11:16 +0800

    Update list-menu.js

--- plugins/list-menu.js
+++ plugins/list-menu.js
@@ -29,8 +29,8 @@ const listMessage = {
   sections
 }
 
-  if(!text) return conn.sendMessage(m.chat, listMessage, { quoted: ftoko })
-  if (!ar.includes(text)) throw conn.sendMessage(m.chat, listMessage, { quoted: ftoko })
+  if(!text) return conn.sendMessage(m.chat, listMessage, { quoted: m })
+  if (!ar.includes(text)) throw conn.sendMessage(m.chat, listMessage, { quoted: m })
   if (!res.ok) throw `${res.status} ${res.statusText}`
   let json = await res.json()
   if (!json.image) throw json
