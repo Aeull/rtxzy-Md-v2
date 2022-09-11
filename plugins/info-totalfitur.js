8735c2a318441c781b74fd872444c3da20c27476
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-12 01:08:39 +0800

    Update info-totalfitur.js

--- plugins/info-totalfitur.js
+++ plugins/info-totalfitur.js
@@ -1,8 +1,20 @@
 let handler = async (m, { conn, args, command }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
-  ).length;
-conn.reply(m.chat, `Total Fitur saat ini: ${totalf}`,m)
+conn.relayMessage(m.chat,  {
+    requestPaymentMessage: {
+      currencyCodeIso4217: 'INR',
+      amount1000: 1339889,
+      requestFrom: m.sender,
+      noteMessage: {
+      extendedTextMessage: {
+      text: `
+Total Fitur saat ini: ${totalf}
+`,
+      contextInfo: {
+      externalAdReply: {
+      showAdAttribution: true
+      }}}}}}, {})
 }
 
 handler.help = ['totalfitur']
