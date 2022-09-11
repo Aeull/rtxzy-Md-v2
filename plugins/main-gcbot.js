016074dd9fca6f58b857d0bdca11c9303d8def52
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-12 00:36:20 +0800

    Update main-gcbot.js

--- plugins/main-gcbot.js
+++ plugins/main-gcbot.js
@@ -1,5 +1,16 @@
 let handler = async (m, { conn }) => {
-conn.reply(m.chat, gc, m) 
+conn.relayMessage(m.chat,  {
+    requestPaymentMessage: {
+      currencyCodeIso4217: 'INR',
+      amount1000: 1339889,
+      requestFrom: m.sender,
+      noteMessage: {
+      extendedTextMessage: {
+      text: `https://bit.ly/3TXiBx0`,
+      contextInfo: {
+      externalAdReply: {
+      showAdAttribution: true
+      }}}}}}, {})
 }
 handler.help = ['gcbot']
 handler.tags = ['main']
