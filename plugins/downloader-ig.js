f9a84e381549dbb3b2bc4c3b87f27b0fa5fad289
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-12 14:42:25 +0800

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, args }) => {
  // let url = `https://api.lolhuman.xyz/api/instagram?apikey=7facd6f11077ee4daecd55d5&url=${args[0]}`
     
 let txt = `🚀 *Link:* ${await(await axios.get(`https://tinyurl.com/api-create.php?url=${args[0]}`)).data}` 
-    await conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/instagram?apikey=7facd6f11077ee4daecd55d5&url=${args[0]}` , txt, wm, `No Wm`, `.tiktoknowm ${args[0]}`, `Audio`, `.tiktokaudio ${args[0]}`, m)
+    await conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/instagram?apikey=7facd6f11077ee4daecd55d5&url=${args[0]}` , txt, wm, m)
 }
 handler.help = ['instagram'].map(v => v + ' <url>')
 handler.tags = ['downloader']
